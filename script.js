58d31aacc5cedb7cb6795ab9facbe26744786b01
Ivan Martins <ivanmartins.ilha@gmail.com>
2023-11-19 20:46:15 -0300

    change setion models and styles

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const openMenuMobile = document.querySelector(".icon-button-header");
 const containerMenuMobile = document.querySelector(".nav-mobile");
 const closeMenu = document.querySelector(".close-menu");
-const navMobile = document.querySelector(".link")
+const navMobile = document.querySelector(".link");
 
+// modal mobile
 openMenuMobile.addEventListener("click", () => {
   containerMenuMobile.style.display = "block";
   console.log(containerMenuMobile);
 });
 
 containerMenuMobile.addEventListener("click", (event) => {
-  
   const elementClicked = event.target.classList[0];
-
   const classNames = [
     "link-models",
     "link-history",
@@ -28,7 +27,9 @@ containerMenuMobile.addEventListener("click", (event) => {
     containerMenuMobile.style.display = "none";
   }
 });
+// modal mobile
 
+// navegação de links internos
 const menuLinks = document.querySelector(".nav-footer");
 
 menuLinks.addEventListener("click", (event) => {
@@ -63,7 +64,9 @@ function smoothScrollTo(endX, endY, duration) {
     window.scroll(newX, newY);
   }, 1000 / 60);
 }
+// navegação de links internos
 
+// delegation para deslocamentos de cards models
 const containerModels = document.querySelector("#models");
 const next = document.querySelector(".next");
 const models02 = document.querySelector(".s2");
@@ -86,13 +89,40 @@ containerModels.addEventListener("click", (event) => {
     (element) => element === elementClicked
   );
   if (isOkOne) {
-    effectTransition(models02, "-150%");
+    effectTransition(models02, "-16%");
     return;
   }
   if (isOkTwo) {
     effectTransition(models01, "3%");
   }
 });
+// delegation para deslocamentos de cards models
+
+// evento para lidar com abertura de texto de cards models
+// const buttonInfo = document.querySelectorAll(".button-info");
+// const containerModel = document.querySelector(".container-models");
+// const textModels = document.querySelectorAll(".container-models .information .description p");
+
+// buttonInfo.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     containerModel.classList.toggle("active");
+
+//     if (containerModel.classList.contains("active")) {
+//       button.textContent = "LER MENOS";
+//       textModels.forEach((text) => {
+//         text.style.maxWidth = "70%";
+//         text.style.whiteSpace = "wrap"
+//       })
+//     } else {
+//       button.textContent = "LER MAIS";
+//       textModels.forEach((text) =>{
+//         text.style.maxWidth = "15ch"
+//         text.style.whiteSpace = "nowrap"
+//       }) ;
+//     }
+//   });
+// });
+
 
 const buttonInfo = document.querySelectorAll(".button-info");
 const extractButton = Array.from(buttonInfo);
@@ -100,30 +130,81 @@ const buttonFirstCard = buttonInfo[0];
 const buttonsecondCard = buttonInfo[1];
 const clouseInformation = document.querySelector(".fechar");
 const containerModel = document.querySelector(".container-models");
+const textModels = document.querySelectorAll(".container-models .information .description p");
 
-buttonFirstCard.addEventListener("click", () => {
-  console.log("aqui");
-  containerModel.classList.toggle("active");
-  const isActiveClass = containerModel.classList.contains("active");
 
-  if (isActiveClass) {
-    buttonFirstCard.textContent = "LER MENOS";
-    return;
-  }
-  buttonFirstCard.textContent = "LER MAIS";
-});
-buttonsecondCard.addEventListener("click", () => {
-  models02.classList.toggle("active");
-  const isActiveClass = models02.classList.contains("active");
+// const showDesciption = (showtext, hiddenText, index) =>{
+//   containerModel.classList.toggle("active");
+//   console.log('ShowDesciption');
+//   const isActiveClass = containerModel.classList.contains("active")
+//   if (isActiveClass) {
+//     buttonFirstCard.textContent = showtext;
+//         textModels[index].style.maxWidth = "90%";
+//         textModels[index].style.whiteSpace = "wrap";
+//         return
+//   }
+//   buttonFirstCard.textContent = hiddenText;
+//   textModels[index].style.maxWidth = "25ch";
+//   textModels[index].style.whiteSpace = "nowrap";
+// }
 
-  if (isActiveClass) {
-    buttonsecondCard.textContent = "LER MENOS";
-    return;
-  }
-  buttonsecondCard.textContent = "LER MAIS";
-});
 
-// animation
+// buttonInfo[0].addEventListener('click', () =>{
+//   containerModel.classList.toggle("active");
+//   const isActiveClass = containerModel.classList.contains("active")
+//   if (isActiveClass) {
+//     buttonFirstCard.textContent = "LER MENOS";
+//         textModels[0].style.maxWidth = "90%";
+//         textModels[0].style.whiteSpace = "wrap";
+//       return
+//   }
+//       buttonFirstCard.textContent = "LER MAIS"
+//       textModels[0].style.maxWidth = "25ch";
+//       textModels[0].style.whiteSpace = "nowrap";
+   
+  
+// })
+
+// buttonsecondCard.addEventListener("click", () => {
+//   containerModel.classList.toggle("active");
+//   const isActiveClass = containerModel.classList.contains("active");
+//   if (isActiveClass) {
+//     buttonFirstCard.textContent = "LER MENOS";
+//     textModels[1].style.maxWidth = "90%";
+//     textModels[1].style.whiteSpace = "wrap";
+//     return;
+//   }
+//   buttonFirstCard.textContent = "LER MAIS";
+//   textModels[1].style.maxWidth = "25ch";
+//   textModels[1].style.whiteSpace = "nowrap";
+// });
+// buttonFirstCard.addEventListener("click", () => {
+//   containerModel.classList.toggle("active");
+//   const isActiveClass = containerModel.classList.contains("active");
+
+//   if (isActiveClass) {
+//     buttonFirstCard.textContent = "LER MENOS";
+//     textModels.forEach(
+//       (text) => text.style.display = "block"
+//     )
+//     return;
+//   }
+//   buttonFirstCard.textContent = "LER MAIS";
+//    textModels.forEach((text) => (text.style.display = "none"));
+// });
+// buttonsecondCard.addEventListener("click", () => {
+//   models02.classList.toggle("active");
+//   const isActiveClass = models02.classList.contains("active");
+
+//   if (isActiveClass) {
+//     buttonsecondCard.textContent = "LER MENOS";
+//     return;
+//   }
+//   buttonsecondCard.textContent = "LER MAIS";
+// });
+// evento para lidar com abertura de texto de cards models
+
+// animation para os texto de entrada e saida da tela
 const observerElement = new IntersectionObserver((elements) => {
   elements.forEach((element) => {
     if (element.isIntersecting) {
@@ -138,3 +219,4 @@ const elementsHidden = document.querySelectorAll(".hidden");
 elementsHidden.forEach((elementHidden) =>
   observerElement.observe(elementHidden)
 );
+// animation para os texto de entrada e saida da tela
